5df3237f21c7365220a2e31929a2d998ff2b5987
Jason L. van Brackel <jason@vanbrackel.net>
2019-08-07 11:24:19 -0400

    Add variables

--- automate.sh
+++ automate.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Variables
+resource_group_name="automate-all-things"
+service_principal_name="TheRanchCast"
+
 # Create our AD Group for a Least Privlege Serivce Principal
-resource_group=$(az group create -l eastus2 -n jvb-automate-all-things)
+resource_group=$(az group create -l eastus2 -n $resource_group_name)
 
 # Create the Service Principal scoped to the resource group
-service_principal=$(az ad sp create-for-rbac -n "TheRanchCast" --role contributor --scopes $(echo "$resource_group" | jq .id -r))
+service_principal=$(az ad sp create-for-rbac -n $service_principal_name --role contributor --scopes $(echo "$resource_group" | jq .id -r))
 
 # Login to Azure with the Service Principal
 az login --service-principal -u $(echo $service_principal | jq .appId -r) -p $(echo $service_principal | jq .password -r) --tenant $(echo $service_principal | jq .tenant -r)
