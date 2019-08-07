1de6af7a2b815fb7bddab370da5487c60c3e0e64
Jason L. van Brackel <jason@vanbrackel.net>
2019-08-07 11:21:18 -0400

    Fix SP creation from The RanchCast Episode 3

--- automate.sh
+++ automate.sh
@@ -4,8 +4,9 @@
 resource_group=$(az group create -l eastus2 -n jvb-automate-all-things)
 
 # Create the Service Principal scoped to the resource group
+service_principal=$(az ad sp create-for-rbac -n "TheRanchCast" --role contributor --scopes $(echo "$resource_group" | jq .id -r))
 
 # Login to Azure with the Service Principal
-az login --service-principal -u CLIENT_ID -p CLIENT_SECRET --tenant TENANT_ID
+az login --service-principal -u $(echo $service_principal | jq .appId -r) -p $(echo $service_principal | jq .password -r) --tenant $(echo $service_principal | jq .tenant -r)
 
 # Episode 4
--- bootstrap-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create our AD Group for a Least Privlege Serivce Principal
-resource_group=$(az group create -l eastus2 -n jvb-automate-all-things)
-
