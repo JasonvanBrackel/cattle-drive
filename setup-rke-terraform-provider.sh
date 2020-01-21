08763732c30a55b0ab550ed73a59c89335c8ab7c
Jason van Brackel <jason@vanbrackel.net>
2020-01-21 11:24:26 -0500

    Fix RKE Terraform provider setup

--- setup-rke-terraform-provider.sh
+++ setup-rke-terraform-provider.sh
@@ -3,13 +3,13 @@
 # Download the provider
 TERRAFORM_VERSION="0.14.1"
 ARCH="linux-amd64"
-curl  -L "https://github.com/yamamoto-febc/terraform-provider-rke/releases/download/$TERRAFORM_VERSION/terraform-provider-rke_$(echo $TERRAFORM_VERSION)_$ARCH.zip" -o terraform_provider_rke.zip
+curl  -L "https://github.com/rancher/terraform-provider-rke/releases/download/$TERRAFORM_VERSION/terraform-provider-rke_$(echo $TERRAFORM_VERSION)_$ARCH.zip" -o terraform_provider_rke.zip
 
 # Unzip
 unzip terraform_provider_rke.zip
 
 # Move to the plugins directory
-mv "terraform-provider-rke_v$TERRAFORM_VERSION" ~/.terraform.d/plugins/ 
+mv "terraform-provider-rke_v$TERRAFORM_VERSION" ~/.terraform.d/plugins/terraform-provider-rke
 
 # Cleanup
 rm terraform_provider_rke.zip
